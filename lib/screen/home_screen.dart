62a243208d2e529fe587ac3cf607b19a3550573e
Seang Soanr <sonayaya92@gmail.com>
2026-05-05 17:23:04 +0700

    Remove mainAxisAlignment from Column widget

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          
           children: const [Text("Welcome to the Home Screen")],
         ),
       ),
